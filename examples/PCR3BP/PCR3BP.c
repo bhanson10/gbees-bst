641c01e5a36e471b761ec86c05a940a7c14153da
Benjamin Hanson <blhanson@ucsd.edu>
2024-08-17 17:28:18 -0700

    Include possibility of differing measurement dimensionality

--- examples/PCR3BP/PCR3BP.c
+++ examples/PCR3BP/PCR3BP.c
@@ -1,20 +1,24 @@
 #include "../../gbees.c"
 
-#define DIM 4
+#define DIM_f 4 // State dimension
+#define DIM_h 4 // Measurement dimension
 
 // This function defines the dynamics model - required
-void PCR3BP(double* x, double* dx, double* coef){
-    double* v = (double*)malloc(DIM * sizeof(double)); 
+void PCR3BP(double* f, double* x, double* dx, double* coef){
     double r1 = pow(pow(x[0]+coef[0],2)+pow(x[1],2),1.5);
     double r2 = pow(pow(x[0]-1+coef[0],2)+pow(x[1],2),1.5);
-    v[0] = x[2];
-    v[1] = x[3];
-    v[2] = 2*x[3]+x[0]-(coef[0]*(x[0]-1+coef[0])/r2)-((1-coef[0])*(x[0]+coef[0])/r1);
-    v[3] = -2*x[2]+x[1]-(coef[0]*x[1]/r2)-((1-coef[0])*x[1]/r1);
-    for(int i = 0; i < DIM; i++){
-        x[i] = v[i];
-    }
-    free(v); 
+    f[0] = x[2];
+    f[1] = x[3];
+    f[2] = 2*x[3]+x[0]-(coef[0]*(x[0]-1+coef[0])/r2)-((1-coef[0])*(x[0]+coef[0])/r1);
+    f[3] = -2*x[2]+x[1]-(coef[0]*x[1]/r2)-((1-coef[0])*x[1]/r1);
+}
+
+// This function defines the measurement model - required
+void identity(double* h, double* x, double* dx, double* coef){
+    h[0] = x[0];
+    h[1] = x[1];
+    h[2] = x[2];
+    h[3] = x[3];
 }
 
 // This function defines the initial grid boundaries - optional
@@ -29,25 +33,26 @@ int main(){
     //=================================== Read in initial discrete measurement =================================//
     printf("\nReading in initial discrete measurement...\n\n");
 
-    char* P_DIR = "<path_to_pdf>";    // Saved PDFs path
-    char* M_DIR = "./";               // Measurement path
-    char* M_FILE = "measurement.txt"; // Measurement file
-    Meas M = Meas_create(DIM, M_DIR, M_FILE);
+    char* P_DIR = "<path_to_pdf>";       // Saved PDFs path
+    char* M_DIR = "./measurements";    // Measurement path
+    char* M_FILE = "measurement0.txt"; // Measurement file
+    Meas M = Meas_create(DIM_h, M_DIR, M_FILE);
     //==========================================================================================================//
 
     //========================================== Read in user inputs ===========================================//
     printf("Reading in user inputs...\n\n");
 
-    double del[DIM];                              // Grid width, default is half of the std. dev. from the initial measurement 
-    for(int i = 0; i < DIM; i ++){
+    double del[DIM_f];                              // Grid width, default is half of the std. dev. from the initial measurement 
+    for(int i = 0; i < DIM_f; i ++){
         del[i] = pow(M.cov[i][i],0.5)/2;
     }
-    Grid G = Grid_create(DIM, 1E-7, M.mean, del); // Inputs: (dimension, probability threshold, center, grid width)       
+    Grid G = Grid_create(DIM_f, 1E-7, M.mean, del); // Inputs: (dimension, probability threshold, center, grid width)       
 
-    double coef[] = {2.528017528540000E-5};       // PCR3BP trajectory attributes (mu)
+    double coef[] = {1.901109735892602E-07};       // PCR3BP trajectory attributes (mu)
     Traj T = Traj_create(1, coef);                // Inputs: (# of coefficients, coefficients)
-    int NUM_DIST = 17;                            // Number of distributions recorded per measurement
-    int NUM_MEAS = 1;                             // Number of measurements
+
+    int NUM_DIST = 8;                             // Number of distributions recorded per measurement
+    int NUM_MEAS = 4;                             // Number of measurements
     int DEL_STEP = 20;                            // Number of steps per deletion procedure
     int OUTPUT_FREQ = 20;                         // Number of steps per output to terminal
     bool OUTPUT = false;                          // Write info to terminal
@@ -57,7 +62,7 @@ int main(){
     //==========================================================================================================//
 
     //================================================= GBEES ==================================================//
-    run_gbees(PCR3BP, PCR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, DIM, OUTPUT, RECORD, MEASURE, BOUNDS);
+    run_gbees(PCR3BP, identity, PCR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, OUTPUT, RECORD, MEASURE, BOUNDS);
 
     return 0;
 }
--- examples/PCR3BP/PCR3BP.py
+++ examples/PCR3BP/PCR3BP.py
@@ -2,7 +2,8 @@ import sys
 sys.path.append('../../')
 import gbeespy as gbees  # type: ignore
 
-DIM = 4
+DIM_f = 4 # State dimension
+DIM_h = 4 # Measurement dimension
 
 # This function defines the dynamics model - required
 def PCR3BP(x, dx, coef):
@@ -14,6 +15,14 @@ def PCR3BP(x, dx, coef):
     v4 = -2*x[2]+x[1]-(coef[0]*x[1]/r2)-((1-coef[0])*x[1]/r1)
     return [v1, v2, v3, v4]
 
+# This function defines the measurement model - required
+def identity(x, dx, coef):
+    v1 = x[0]
+    v2 = x[1]
+    v3 = x[2]
+    v4 = x[3]
+    return [v1, v2, v3, v4]
+
 # This function defines the initial grid boundaries - optional
 def PCR3BP_J(x, coef):
     r1 = ((x[0]+coef[0])**2+(x[1])**2)**0.5
@@ -22,29 +31,30 @@ def PCR3BP_J(x, coef):
     return J
 
 #==================================== Read in initial discrete measurement ==================================#
-print("\nReading in initial discrete measurement...\n\n")
+print("Reading in initial discrete measurement...\n")
 
-P_DIR = "<path_to_pdf>"     # Saved PDFs path
-M_DIR = "./"                # Measurement path
-M_FILE = "measurement.txt"; # Measurement file
-M = gbees.Meas_create(DIM, M_DIR, M_FILE)
+P_DIR = "<path_to_pdf>"      # Saved PDFs path
+M_DIR = "./measurements"     # Measurement path
+M_FILE = "measurement0.txt"; # Measurement file
+M = gbees.Meas_create(DIM_h, M_DIR, M_FILE)
 #============================================================================================================#
 
 #=========================================== Read in user inputs ============================================#
-print("Reading in user inputs...\n\n")
+print("Reading in user inputs...\n")
 
-dx = [None] * DIM                             # Grid width, default is half of the std. dev. from the initial measurement 
-for i in range(DIM):
+dx = [None] * DIM_h                             # Grid width, default is half of the std. dev. from the initial measurement 
+for i in range(DIM_h):
     dx[i] = (M.cov[i][i]**(0.5))/2
-G = gbees.Grid_create(DIM, 1E-7, M.mean, dx); # Inputs: (dimension, probability threshold, center, grid width)    
+G = gbees.Grid_create(DIM_h, 1E-7, M.mean, dx); # Inputs: (dimension, probability threshold, center, grid width)    
  
-coef = [2.528017528540000E-5]                 # PCR3BP trajectory attributes (mu)
+# coef = [2.528017528540000E-5]                 # PCR3BP trajectory attributes (mu)
+coef = [1.901109735892602E-07]                 # PCR3BP trajectory attributes (mu)
 T = gbees.Traj_create(len(coef), coef);       # Inputs: (# of coefficients, coefficients)
 
-OUTPUT_FREQ = 20;                             # Number of steps per output to terminal
+NUM_DIST = 8;                                # Number of distributions recorded per measurement
+NUM_MEAS = 4;                                 # Number of measurements
 DEL_STEP = 20;                                # Number of steps per deletion procedure
-NUM_DIST = 17;                                # Number of distributions recorded per measurement
-NUM_MEAS = 1;                                 # Number of measurements
+OUTPUT_FREQ = 20;                             # Number of steps per output to terminal
 OUTPUT = False;                               # Write info to terminal
 RECORD = True;                                # Write PDFs to .txt file
 MEASURE = True;                               # Take discrete measurement updates
@@ -52,4 +62,4 @@ BOUNDS = True;                                # Add inadmissible regions to grid
 #============================================================================================================#
 
 #================================================== GBEES ===================================================#
-gbees.run_gbees(PCR3BP, PCR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, DIM, OUTPUT, RECORD, MEASURE, BOUNDS)
+gbees.run_gbees(PCR3BP, identity, PCR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, OUTPUT, RECORD, MEASURE, BOUNDS)
